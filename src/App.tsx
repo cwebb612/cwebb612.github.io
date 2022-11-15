a9e24840470115eb6e484b29610f808e26ac72a8
cwebb612 <charlie.webb2024@gmail.com>
2022-11-15 14:01:57 -0500

    fixes from default to launch material UI

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
